34f1bcf15f0730012d696cfd3751dc35d902094a
esprajwol <prajwolpkc@outlook.com>
2022-08-26 11:52:29 +0545

    add compiled dist

---
+++ dist/decorators/tenancy.decorator.d.ts
@@ -0,0 +1,2 @@
+export declare const InjectTenancyModel: (model: string) => (target: object, key: string | symbol, index?: number | undefined) => void;
+export declare const InjectTenancyConnection: (name?: string | undefined) => (target: object, key: string | symbol, index?: number | undefined) => void;
